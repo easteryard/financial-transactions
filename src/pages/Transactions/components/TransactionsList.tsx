4169c46120a635ea161a9a2d7ac52a73ef57392b
Casper Østergaard <casper@acto.dk>
2021-03-26 01:26:48 +0100

    Testing push

--- src/pages/Transactions/components/TransactionsList.tsx
+++ src/pages/Transactions/components/TransactionsList.tsx
@@ -195,6 +195,7 @@ export const TransactionsList = ({ data, handleDeletion }: IProps) => {
         </StyledTableBody>
       </StyledTable>
       <Tooltip id='header-cell' message='Hold shift to sort multiple' />
+      <p>hey</p>
     </StyledTransactions>
   );
 };
